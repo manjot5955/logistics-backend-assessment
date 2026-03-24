fd0350e0733509c979345581c059574c0e6f3885
MJ <you@example.com>
2026-03-24 23:54:21 +0530

    redundancy removed for sequelize authenticate

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,5 @@
-const { Sequelize } = require("sequelize");
 require("dotenv").config();
+const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
@@ -9,15 +9,7 @@ const sequelize = new Sequelize(
     host: process.env.DB_HOST,
     dialect: "mysql",
     logging: false,
-  },
+  }
 );
 
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log("✅ Connection has been established successfully.");
-  })
-  .catch((error) => {
-    console.error("❌ Unable to connect to the database:", error);
-  });
 module.exports = sequelize;
--- src/server.js
+++ src/server.js
@@ -1,29 +1,23 @@
 require("dotenv").config();
 const sequelize = require("./config/database");
 const app = require("./app");
-require("./models");
-
+require("./models"); 
 const PORT = process.env.PORT || 3000;
 
 async function startServer() {
   try {
-    console.log("Connected to DB:", process.env.DB_NAME);
-    console.log("DB USER:", process.env.DB_USER);
-    await sequelize.authenticate();
-    console.log("✅ Database connected");
+    console.log("Connecting to DB...");
 
-    // try {
-    //     await sequelize.sync({ alter: true });
-    //     console.log("Tables synced");
-    // } catch (error) {
-    //     console.log("Tables synced errorrrrrr", error);
-    // }
+    await sequelize.authenticate();
+    console.log("✅ Database connected successfully");
 
     app.listen(PORT, () => {
       console.log(`🚀 Server running on port ${PORT}`);
     });
+
   } catch (error) {
     console.error("❌ Unable to connect to DB:", error.message);
+    process.exit(1); 
   }
 }
 
